e0cf84339d09ba221070f0c2029b6cb43e43d185
hels <1597445096@qq.com>
2021-02-04 16:54:08 +0800

    first commit

---
+++ src/main/java/com/cn/MybatisHelloWorld.java
@@ -0,0 +1,31 @@
+package com.cn;
+
+import com.cn.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * @description:
+ * @author: helisen
+ * @create: 2021-01-28 15:48
+ **/
+public class MybatisHelloWorld {
+    public static void main(String[] args) {
+        String resource = "mybatis/Configuration.xml";
+        Reader reader;
+        try {
+            reader = Resources.getResourceAsReader(resource);
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            SqlSession session = sqlSessionFactory.openSession();
+            User user = session.selectOne("com.cn.mapper.UserMapper.getUser", 19);
+            System.out.println(user.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/cn/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.cn.mapper;
+
+import com.cn.pojo.User;
+
+/**
+ * @description:
+ * @author: helisen
+ * @create: 2021-01-28 16:02
+ **/
+public interface UserMapper {
+
+    User getUser(Integer id);
+}
